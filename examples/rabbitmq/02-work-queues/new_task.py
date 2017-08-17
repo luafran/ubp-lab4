da9accdc1b589edf7e67218ffff219e5ec9ffb92
Luciano Afranllie Ruiz <luciano.afranllie.ruiz@intel.com>
2017-08-17 09:33:23 -0300

    add rabbitmq examples

---
+++ examples/rabbitmq/02-work-queues/new_task.py
@@ -0,0 +1,14 @@
+import pika
+import sys
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1'))
+channel = connection.channel()
+
+channel.queue_declare(queue='task_queue', durable=True)
+
+message = ' '.join(sys.argv[1:]) or "Hello World!"
+channel.basic_publish(exchange='', routing_key='task_queue',
+                      body=message, properties=pika.BasicProperties(delivery_mode=2))
+
+print(" [x] Sent %r" % message)
+connection.close()
